caf1078ebec8d04d708a440eb33331d1cba1239a
GitdoPedro <emailpedrolima@gmail.com>
2023-05-07 19:33:58 -0300

    [M3S01] Ex 03 - Revisão API REST com Testes (parte 2)

---
+++ src/main/java/com/example/M3S01/controller/VeiculosController.java
@@ -0,0 +1,68 @@
+package com.example.M3S01.controller;
+
+
+import com.example.M3S01.model.Veiculo;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/veiculos")
+@CrossOrigin
+public class VeiculosController {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private VeiculoService service;
+
+    @PostMapping
+    public ResponseEntity inserir(@RequestBody @Valid VeiculoRequest request) {
+        Veiculo veiculo = modelMapper.map(request, Veiculo.class);
+        veiculo = service.inserir(veiculo);
+        VeiculoResponse resp = modelMapper.map(veiculo, VeiculoResponse.class);
+        return ResponseEntity.created(URI.create(resp.getId().toString())).body(resp);  // 201
+    }
+
+    @GetMapping
+    public ResponseEntity<List<VeiculoResponse>> consultar() {
+        List<Veiculo> veiculos = service.consultar();
+        List<VeiculoResponse> resp = veiculos.stream()
+                .map(p -> modelMapper.map(p, VeiculoResponse.class)).toList();
+        return ResponseEntity.ok(resp);
+    }
+
+    @GetMapping("{placa}")
+    public ResponseEntity<VeiculoResponse> consultar(@PathVariable("placa") Integer placa) {
+        Veiculo veiculo  = service.consultar(placa);
+        VeiculoResponse resp = modelMapper.map(veiculo, VeiculoResponse.class);
+        return ResponseEntity.ok(resp);
+    }
+
+    @DeleteMapping("{placa}")
+    public ResponseEntity excluir(@PathVariable("placa") Integer placa) {
+        service.excluir(placa);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{placa}/multas")
+    public ResponseEntity<VeiculoResponse> atualizarQtdMultas(@PathVariable String placa, @RequestBody @Valid VeiculoRequest request) {
+        Veiculo veiculoAtualizado = service.AcrescentaMultas(placa, request.getQtdMultas());
+        VeiculoResponse resp = modelMapper.map(veiculoAtualizado, VeiculoResponse.class);
+        return ResponseEntity.ok(resp);
+    }
+
+
+
+
+
+
+
+
+}
